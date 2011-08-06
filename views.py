0b35b8aea1c225da492491551ea8ae1fc5f651db
Henrik Lied <henriklied@gmail.com>
2011-08-06 17:45:47 +0200

    Se

--- views.py
+++ views.py
@@ -81,7 +81,7 @@ def trainer_fetch(request):
     meta = json.loads(sentence.metadata)
     mk = ''
     for key, value in meta.values():
-        mk += '%s: %s -'
+        mk += '%s: %s -' % (key, value)
     mk = mk[:len(mk-2)]
     skeleton = '''
     <div class="sentence" rel="%d">
